c7ce08ccd10a73afb85e843d5d2be74079d5ddfc
Alex Klein <alex.klein@mayvue.com>
2023-12-03 09:46:14 -0500

    Day 3, part 1 solution

---
+++ AdventOfCode/Day03.cs
@@ -0,0 +1,65 @@
+
+
+using System.Text.RegularExpressions;
+
+public class Day03 : BaseDay
+{
+    private readonly List<string> _input;
+    
+    public Day03()
+    {
+        _input = File.ReadAllLines(InputFilePath).ToList();
+    }
+
+//Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    public override ValueTask<string> Solve_1()
+    {
+        List<string> partNumberList = new();
+        Regex regex = new Regex("\\d+");
+        for (int row = 0; row < _input.Count; row++)
+        {
+            var matches = regex.Matches(_input[row]);
+            for (int i=0; i < matches.Count; i++)
+            {
+                var match = matches[i];
+                if (checkIsPartNumber(row, match.Index, match.Index + match.Length - 1))
+                {
+                    partNumberList.Add(match.Value);
+                }
+            }
+        }
+
+        return new(partNumberList.Select(x => Convert.ToInt32(x)).Sum().ToString());
+    }
+
+    private bool checkIsPartNumber(int rowIndex, int startIndex, int endIndex)
+    {
+        var startRow = Math.Clamp(rowIndex - 1, 0, _input.Count - 1);
+        var endRow = Math.Clamp(rowIndex + 1, 0, _input.Count - 1);
+        var startCol = Math.Clamp(startIndex - 1, 0, _input[rowIndex].Length - 1);
+        var endCol = Math.Clamp( endIndex + 1, 0, _input[rowIndex].Length - 1);
+
+        for (int row=startRow; row <= endRow; row++)
+        {
+            for (int col = startCol; col <= endCol; col++)
+            {
+                if (checkIsSymbolChar(_input[row][col]))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private bool checkIsSymbolChar(char input)
+    {
+        return input != '.' && (input < '0' || input > '9');
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+}
