a14f31937e9047bdc9ed492686cf3b18550131cc
Alex Klein <alex.klein@mayvue.com>
2023-12-09 23:52:14 -0500

    Day 9 solutions

---
+++ AdventOfCode/Day09.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+public class Day09 : BaseDay
+{
+    private readonly List<string> _input;
+
+    public Day09()
+    {
+        _input = File.ReadAllLines(InputFilePath).ToList();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var sequences = _input.Select(line => line.Split(" ").Select(numStr => Convert.ToInt32(numStr)).ToList()).ToList();
+        var nextValues = sequences.Select(x => GetNext(x)).ToList();
+        return new(nextValues.Sum().ToString());
+    }
+
+    private int GetPrevious(List<int> input)
+    {
+        if (input.All(x => x == 0)) return 0;
+        else return input.First() - GetPrevious(GetDerivative(input));
+    }
+    private int GetNext(List<int> input)
+    {
+        if (input.All(x => x == 0)) return 0;
+        else return input.Last() + GetNext(GetDerivative(input));
+    }
+
+    private List<int> GetDerivative(List<int> input)
+    {
+        return input.Take(input.Count - 1).Zip(input.Skip(1), (a, b) => b - a).ToList();
+    }
+    public override ValueTask<string> Solve_2()
+    {
+        var sequences = _input.Select(line => line.Split(" ").Select(numStr => Convert.ToInt32(numStr)).ToList()).ToList();
+        var nextValues = sequences.Select(x => GetPrevious(x)).ToList();
+        return new(nextValues.Sum().ToString());
+    }
+}
